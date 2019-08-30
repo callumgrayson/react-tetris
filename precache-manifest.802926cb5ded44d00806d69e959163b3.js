7a5f819afdfcf9bb1c7eff8817f5c98f943263af
callumgrayson <callum@musiio.com>
2019-08-30 11:27:19 +0800

    Updates

---
+++ precache-manifest.802926cb5ded44d00806d69e959163b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb187058271e4936e179c0afea7140d",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "b48adbecb1466bece189",
+    "url": "/react-tetris/static/css/main.c2d927d8.chunk.css"
+  },
+  {
+    "revision": "3297d7606277eb67b7b4",
+    "url": "/react-tetris/static/js/2.66851ac1.chunk.js"
+  },
+  {
+    "revision": "b48adbecb1466bece189",
+    "url": "/react-tetris/static/js/main.2ab26f21.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
